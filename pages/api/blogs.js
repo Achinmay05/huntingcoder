8f6ed47a648dbd96ad859bb5e039610df1f34091
Chinmay Awasthi <chinmayawasthi05@gmail.com>
2024-07-26 14:08:08 +0530

    created api endpoints

---
+++ pages/api/blogs.js
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import * as fs from 'fs';
+
+export default function handler(req, res) {
+  fs.readdir("blogdata", (err, data) => {
+    console.log(data)
+    res.status(200).json(data); //converting string to object.
+  })
+}
---
+++ pages/api/getblogs.js
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+// http://localhost:3000/api/getblogs?slug=how-to-learn-javascript
+
+import * as fs from 'fs';
+
+export default function handler(req, res) {
+  fs.readFile(`blogdata/${req.query.slug}.json`, 'utf-8', (err, data) => {
+    if (err) {
+      res.status(500).json({ error: "No such Blog found !" })
+    }
+    console.log('')
+    res.status(200).json(JSON.parse(data)); //converting string to object.
+  })
+}
