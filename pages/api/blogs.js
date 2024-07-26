64f97811dd06f47e9a9107cbdd898553e467c0bf
Chinmay Awasthi <chinmayawasthi05@gmail.com>
2024-07-26 19:41:36 +0530

    added functionality to components

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,9 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from 'fs';
 
-export default function handler(req, res) {
-  fs.readdir("blogdata", (err, data) => {
-    console.log(data)
-    res.status(200).json(data); //converting string to object.
-  })
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir("blogdata");
+  let myfile;
+  let allBlogs = [];
+  for (let index = 0; index < data.length; index++) {
+    const item = data[index];
+    console.log(item)
+    myfile = await fs.promises.readFile(('blogdata/' + item), 'utf-8')
+    allBlogs.push(JSON.parse(myfile)) //converting string into object
+  }
+  res.status(200).json(allBlogs)
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,36 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from "@/styles/Blog.module.css";
 import Link from 'next/link';
 const Blog = () => {
+    const [blogs, setBlogs] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:3000/api/blogs').then((a) => {
+            return a.json();
+        }).then((parsed) => {
+            console.log(parsed)
+            setBlogs(parsed)
+        })
+    }, [])
+
     return (
         <div className={styles.container}>
             <main className={styles.main}>
-            <div className={styles.blogs}>
-                <div className={styles.blogItem}>
-                    <Link href={'/blogpost/learn-javascript'}>
-                    <h3>How to learn javascript in 2024?</h3>
-                    </Link>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
-                </div>
-            </div>
-            <div className={styles.blogs}>
-                <div className={styles.blogItem}>
-                    <h3>How to learn javascript in 2024?</h3>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
-                </div>
-            </div>
-            <div className={styles.blogs}>
-                <div className={styles.blogItem}>
-                    <h3>How to learn javascript in 2024?</h3>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
-                </div>
-            </div>
-            <div className={styles.blogs}>
-                <div className={styles.blogItem}>
-                    <h3>How to learn javascript in 2024?</h3>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
-                </div>
-            </div>
+                {blogs.map((blogitem) => {
+                    return <div className={styles.blogItem} key={blogitem.slug}>
+                        <Link href={`/blogpost/{blogitem.slug}`}>
+                            <h3>{blogitem.title}</h3>
+                            <p className={styles.blogItemp}>{blogitem.content.substr(0, 140)}...</p>
+                        </Link>
+
+                    </div>
+                })}
             </main>
         </div>
     )
