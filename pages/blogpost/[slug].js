28b4b4c5556979eeffbc56119081ba8c1f611065
Chinmay Awasthi <chinmayawasthi05@gmail.com>
2024-08-01 23:53:10 +0530

    added Inner HTML

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -4,13 +4,16 @@ import styles from "@/styles/BlogPost.module.css";
 import * as fs from 'fs'
 
 const Slug = (props) => {
+    function createMarkup(content) {
+        return { __html: content };
+    }
+
     const [blog, setBlog] = useState(props.myBlog);
     return <div className={styles.container}>
         <main className={styles.main} >
             <h1 className={styles.title}>{blog && blog.title}</h1>
-            <div className={styles.blogContent}>
-                {blog && blog.content}
-            </div>
+            {blog && <div dangerouslySetInnerHTML={createMarkup(blog.content)} className={styles.blogContent}></div>}
+
         </main>
     </div>
 
@@ -23,7 +26,7 @@ export async function getStaticPaths() {
             { params: { slug: 'how-to-learn-javascript' } },
             { params: { slug: 'how-to-learn-nextjs' } },
         ],
-        fallback: true // false for 'blocking'
+        fallback: false // false for 'blocking'
     };
 }
 
